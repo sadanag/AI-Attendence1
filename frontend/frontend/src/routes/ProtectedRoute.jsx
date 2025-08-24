9bab2d599fbb6cba0bac473878ef6efafaaaa82a
Nagaprasad <140372906+sadanag@users.noreply.github.com>
2025-08-24 17:03:00 +0530

    Update ProtectedRoute.jsx

--- frontend/frontend/src/routes/ProtectedRoute.jsx
+++ frontend/frontend/src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-export default function ProtectedRoute({ roles = ["employee","admin"], children }) {
+export default function ProtectedRoute({ roles = ["employee"], children }) {
   const { user } = useAuth();
   const location = useLocation();
 
